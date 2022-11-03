e4abe5ee34d2a996cd2616802ab0983b3bfdc086
oViE dEv <103589891+OvieDev@users.noreply.github.com>
2022-11-03 22:24:18 +0100

    Added knockback to enemy's attacks (not final)

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,7 +12,9 @@ onready var hitline = $HitLine
 onready var safezone = $Area2D
 onready var effect = $SafezoneEffect
 onready var dashtimer = $DashTimer
+onready var velocitytween = $VelocityTween
 var hits = 0
+var impact = false
 var duck = false
 var can_move = true
 var dash_direction = Vector2.ZERO
@@ -31,72 +33,73 @@ func _input(event):
 			Input.action_press("look_left")
 
 func _physics_process(delta):
+	print(VELOCITY)
 	JUMPING = false
-	VELOCITY = Vector2(0, VELOCITY.y)
-	
-	if Input.is_action_pressed("ui_left"):
-		VELOCITY.x = -100
-		
-	if Input.is_action_pressed("ui_right"):
-		VELOCITY.x = 100
-		
-	if Input.is_action_pressed("ui_up") and after_jump == 0:
-		jump()
-		
-	if Input.is_action_just_pressed("attack") and is_on_floor():
-		attack()
-		
-	if Input.is_action_pressed("ui_down") and is_on_floor() and parry_direction==Vector2.ZERO:
-		duck = true
-		print("Duck")
-	else:
-		duck = false
-		
-	if Input.is_action_pressed("parry") and is_on_floor() and !duck:
-		parry_direction = hitline.cast_to
-		can_move = false
-	elif Input.is_action_just_released("parry"):
-		parry_direction = Vector2.ZERO
-		can_move = true
-	
-	if Input.is_action_just_pressed("dash") and can_move and dash_direction==Vector2.ZERO and FUEL>=10 and roll_direction==Vector2.ZERO:
-		dash_direction = hitline.cast_to*5
-		dash()
-		FUEL-=10
-	if Input.is_action_just_pressed("roll") and can_move and roll_direction==Vector2.ZERO and dash_direction==Vector2.ZERO and is_on_floor():
-		roll_direction.x = hitline.cast_to.x*3.5
-		dash()
-		
-	if Input.is_action_just_pressed("card1"):
-		if cards[0] == true:
-			FUEL = 170
-			cards[0] = false
+	if !impact:
+		VELOCITY = Vector2(0, VELOCITY.y)
+		if Input.is_action_pressed("ui_left"):
+			VELOCITY.x = -100
 			
-	if Input.is_action_just_pressed("card2"):
-		if cards[1] == true:
-			effect.emitting = true
-			for i in safezone.get_overlapping_bodies():
-				if i is Damagable and !i == self:
-					i.emit_signal("damage_received", 999, Vector2.ZERO)
-			cards[1] = false
+		if Input.is_action_pressed("ui_right"):
+			VELOCITY.x = 100
 			
-	if Input.is_action_just_pressed("card3"):
-		if cards[2] == true:
-			hits = -5
+		if Input.is_action_pressed("ui_up") and after_jump == 0:
+			jump()
 		
-	if !JUMPING:
-		if !is_on_floor():
-			VELOCITY.y = lerp(VELOCITY.y, 300, 0.2)
+		if Input.is_action_just_pressed("attack") and is_on_floor():
+			attack()
+		
+		if Input.is_action_pressed("ui_down") and is_on_floor() and parry_direction==Vector2.ZERO:
+			duck = true
+			print("Duck")
 		else:
-			VELOCITY.y = 0
+			duck = false
+		
+		if Input.is_action_pressed("parry") and is_on_floor() and !duck:
+			parry_direction = hitline.cast_to
+			can_move = false
+		elif Input.is_action_just_released("parry"):
+			parry_direction = Vector2.ZERO
+			can_move = true
 	
-	hitline_to_mouse = hitline.position.direction_to(get_local_mouse_position())
-	if Input.is_action_just_pressed("look_right") :#or hitline_to_mouse.x>0:
-		hitline.cast_to.x = 75
-	elif Input.is_action_just_pressed("look_left") :#or hitline_to_mouse.x<0:
-		hitline.cast_to.x = -75
+		if Input.is_action_just_pressed("dash") and can_move and dash_direction==Vector2.ZERO and FUEL>=10 and roll_direction==Vector2.ZERO:
+			dash_direction = hitline.cast_to*5
+			dash()
+			FUEL-=10
+		if Input.is_action_just_pressed("roll") and can_move and roll_direction==Vector2.ZERO and dash_direction==Vector2.ZERO and is_on_floor():
+			roll_direction.x = hitline.cast_to.x*3.5
+			dash()
+		
+		if Input.is_action_just_pressed("card1"):
+			if cards[0] == true:
+				FUEL = 170
+				cards[0] = false
+			
+		if Input.is_action_just_pressed("card2"):
+			if cards[1] == true:
+				effect.emitting = true
+				for i in safezone.get_overlapping_bodies():
+					if i is Damagable and !i == self:
+						i.emit_signal("damage_received", 999, Vector2.ZERO)
+				cards[1] = false
+			
+		if Input.is_action_just_pressed("card3"):
+			if cards[2] == true:
+				hits = -5
+		
+		if !JUMPING:
+			if !is_on_floor():
+				VELOCITY.y = lerp(VELOCITY.y, 300, 0.2)
+			else:
+				VELOCITY.y = 0
 	
-	if dash_direction!=Vector2.ZERO:
+		if Input.is_action_just_pressed("look_right") :
+			hitline.cast_to.x = 75
+		elif Input.is_action_just_pressed("look_left") :
+			hitline.cast_to.x = -75
+	if impact:
+		move_and_slide(VELOCITY, Vector2.UP)
+	elif dash_direction!=Vector2.ZERO:
 		move_and_slide(dash_direction, Vector2.UP)
 	elif roll_direction!=Vector2.ZERO:
 		roll_direction.y = VELOCITY.y
@@ -144,11 +147,7 @@ func add_fuel():
 
 func _on_KinematicBody2D_damage_received(damage, vector):
 	if iframe==0:
-		print(!(vector==Vector2.ZERO or parry_direction.x==vector.x*75))
-		print(!(vector==Vector2.DOWN and duck))
-		print(!(roll_direction==Vector2.ZERO))
 		if ((parry_direction.x*-1==vector.x*75) or (vector!=Vector2.DOWN and duck) or (roll_direction!=Vector2.ZERO)):
-			print("No damage?")
 			hits+=1
 			if parry_direction.x==vector.x*75:
 				enabled_moves[0] = false
@@ -162,6 +161,15 @@ func _on_KinematicBody2D_damage_received(damage, vector):
 			else:
 				iframe = 8
 				handle_iframes()
+				print("completed iframes")
+				velocitytween.interpolate_property(self, "VELOCITY", Vector2.ZERO,
+				vector*150+Vector2(0, -5), 0.25, Tween.TRANS_CIRC,Tween.EASE_OUT)
+				velocitytween.interpolate_property(self, "after_jump", 0, 0, 1)
+				impact = true
+				velocitytween.start()
+				yield(velocitytween, "tween_all_completed")
+				impact = false
+				
 func attack():
 	var target = hitline.get_collider()
 	if target is Damagable and (hits==2 or hits<0):
