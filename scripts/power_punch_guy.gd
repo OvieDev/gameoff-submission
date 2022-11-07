70d7488fbc0a4875e936d08092ba8cdc7d2cf4be
oViE dEv <103589891+OvieDev@users.noreply.github.com>
2022-11-07 19:29:37 +0100

    Added some animations to power punch guy

--- scripts/power_punch_guy.gd
+++ scripts/power_punch_guy.gd
@@ -1,12 +1,14 @@
 extends Enemy
 onready var particles = $JetPackEffect
 onready var to_floor = $ToFloor
+onready var target_raycast = $Target
 var boosting = true
 var able_to_boost_again = 0
+var must_boost = false
 
 func _process(delta):
 	._process(delta)
-	if last_dir==true:
+	if last_dir==false:
 		particles.position = Vector2(-15, 11)
 	else:
 		particles.position = Vector2(13, 11)
@@ -17,14 +19,15 @@ func ai():
 	dist = position.distance_to(player.position)
 	direction_to_player = position.direction_to(player.position)
 	
-	if to_floor.is_colliding():
-		boosting = true
+	target_raycast.cast_to = position.direction_to(player.position)*200
 	
-	if dist>1000 and direction_to_player.y<0:
+	if to_floor.is_colliding() or (is_on_wall() and direction_to_player.y<0):
 		boosting = true
-	
+		
 	if is_on_ceiling():
 		boosting = false
+	
+	must_boost = position.y>player.position.y
 		
 	if boosting:
 		velocity+=Vector2(0, -10)
@@ -32,18 +35,41 @@ func ai():
 		velocity+=Vector2(0, 10)
 		if velocity.y>100:
 			velocity = Vector2(0, 100)
-	if velocity.y<-200 and dist>1000 and direction_to_player.y<0:
-		boosting = true
-	elif velocity.y<-200:
+	if velocity.y<-200 and !must_boost:
 		able_to_boost_again = 120
 		boosting = false
+	elif velocity.y<-150 and must_boost:
+		velocity.y = -150
 	
-	velocity.x = lerp(velocity.x, round(direction_to_player.x)*75, 0.8)
-	
+	velocity.x = direction_to_player.x*75
 	move_and_slide(velocity, Vector2.UP)
+	
+	last_dir = velocity.x<0
 	
 	if !boosting and able_to_boost_again>0:
 		able_to_boost_again-=1
 		if able_to_boost_again==0:
 			boosting = true
 	
+func anim_and_attack():
+	var dir = ""
+	if last_dir:
+		dir = "Left"
+	else:
+		dir = "Right"
+	
+	if attack_tick==0:
+		animation.play("Walk"+dir)
+	else:
+		animation.play("Punch"+dir)
+		
+	if target_raycast.get_collider() is Player and dist<150 and attack_tick==0 and cooldown_tick==0:
+		attack_tick = 120
+		
+	if attack_tick>0:
+		attack_tick-=1
+		if attack_tick==1:
+			cooldown_tick = 180
+	
+	if cooldown_tick>0:
+		cooldown_tick-=1
